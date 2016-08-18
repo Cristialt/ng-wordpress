f988d4697f41514ea45794af5e6d1cec4d02d60a
Cristian Maghiar <cristi@Cristians-iMac.local>
2016-08-18 09:13:43 +0300

    added bower.json; tweaks and fixes to WP REST API

--- app/app.js
+++ app/app.js
@@ -1,27 +1,33 @@
 angular.module('app', ['ngRoute'])
-    
-    .config(function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(true);
 
+    .config(function($routeProvider, $locationProvider) {
         $routeProvider
+
             .when('/', {
                 templateUrl: myLocalized.partials + 'main.html',
                 controller: 'Main'
             })
+
             .when('/:ID', {
                 templateUrl: myLocalized.partials + 'content.html',
                 controller: 'Content'
             });
+
+        $locationProvider.html5Mode({
+            enabled:false,
+            requireBase: false
+        });
     })
 
-    .controller('Main', function($scope, $http, $routeParams) {
-        $http.get('wp-json/posts/').success(function(res){
+    .controller('Main', function($scope, $http) {
+        $http.get('/wp-json/posts/').success(function(res){
             $scope.posts = res;
         });
     })
 
     .controller('Content', function($scope, $http, $routeParams) {
-        $http.get('wp-json/posts/' + $routeParams.ID).success(function(res){
+        $http.get('/wp-json/posts/' + $routeParams.ID).success(function(res){
             $scope.post = res;
+            console.log(res);
         });
     });
--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 
 <html ng-app="app">
-    <head>
-        <base href="/jsonapi/">
-        <title>NG-Wordpress</title>
-        <?php wp_head(); ?>
-    </head>
+<head>
+    <base href="/">
+    <title>NG-Wordpress</title>
+    <?php wp_head(); ?>
+</head>
 
-    <body>
-        <header>
-            <h1>
-                <a href="<?php echo site_url(); ?>">NG-Wordpress Demo Theme</a>
-            </h1>
-        </header>
+<body>
+<header>
+    <h1>
+        <a href="<?php echo site_url(); ?>">NG-Wordpress Demo Theme</a>
+    </h1>
+</header>
 
-        <div ng-view></div>
+<div ng-view></div>
 
-        <footer>
-            &copy; <?php echo date('Y'); ?>
-        </footer>
-    </body>
+<footer>
+    &copy; <?php echo date('Y'); ?>
+</footer>
+</body>
 </html>
